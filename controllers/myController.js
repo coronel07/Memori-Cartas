42ddf2f4a420b20c061c58326d8d86368c5e8ccd
Nicolas <d51702027@alumnos.itr.edu.ar>
2023-05-05 19:15:16 -0300

    conectado con mongoose y creado el documento mediante codigo

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,8 +1,26 @@
 const OneModel = require('../models/myModel');
 const moment = require('moment');
+const Post = require('../models/myModel');
 
 //Ejemplo de respuesta a una petición de tipo GET
 exports.inicio = (req, res) => {
     //crear un documento con mongoose
     res.status(200).render('index');
 };
+
+// Crear un nuevo objeto "Post"
+const newPost = new Post({
+    title: "Mi nuevo post",
+    description: "Por fin anda pa ",
+    date: new Date(),
+    name:'Nicolas'
+  });
+  
+  // Guardar el objeto "Post" en la base de datos
+  newPost.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Nuevo post guardado en la base de datos.");
+    }
+  });
